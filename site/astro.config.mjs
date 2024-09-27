6b22acca735bd178f6de89fe8e2aec4509c2b8e9
Sean <seantsusmilch@gmail.com>
2024-09-27 01:17:21 -0500

    add astro site

---
+++ site/astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig, envField } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  integrations: [tailwind()],
+  experimental: {
+    env: {
+      schema: {
+        PB_URL: envField.string({ context: 'server', access: 'public' }),
+        PB_USER: envField.string({ context: 'server', access: 'public' }),
+        PB_PASS: envField.string({ context: 'server', access: 'public' }),
+      },
+    },
+  },
+});
