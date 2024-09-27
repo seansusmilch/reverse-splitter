c5658786ead56fce75da96e3053a9a92f97f8cf1
Sean <seantsusmilch@gmail.com>
2024-09-27 10:29:28 -0500

    log next executions of jobs

--- reverse_splitter/jobs/scheduler.py
+++ reverse_splitter/jobs/scheduler.py
@@ -2,7 +2,8 @@ from apscheduler.triggers.cron import CronTrigger
 from apscheduler.schedulers.background import BackgroundScheduler
 import reverse_splitter.bin.logger as logger
 import os
-from . import research, send_newsletter
+import reverse_splitter.jobs.research as research
+import reverse_splitter.jobs.send_newsletter as send_newsletter
 
 
 log = logger.setup_logger('Scheduler')
@@ -12,11 +13,11 @@ RESEARCH_CRON = os.getenv('RESEARCH_CRON', '0 2 * * *')
 NEWSLETTER_CRON = os.getenv('NEWSLETTER_CRON', '0 7 * * *')
 
 def schedule_jobs():
-    scheduler.add_job(research.research_job, CronTrigger.from_crontab(RESEARCH_CRON))
-    scheduler.add_job(send_newsletter.send_newsletter_job, CronTrigger.from_crontab(NEWSLETTER_CRON))
+    research_job = scheduler.add_job(research.research_job, CronTrigger.from_crontab(RESEARCH_CRON))
+    newsletter_job = scheduler.add_job(send_newsletter.send_newsletter_job, CronTrigger.from_crontab(NEWSLETTER_CRON))
     log.debug('Jobs scheduled')
     
-    
+    return research_job, newsletter_job
 
 if __name__ == '__main__':
     from datetime import datetime
@@ -24,8 +25,10 @@ if __name__ == '__main__':
     def tick():
         print('Tick! The time is: %s' % datetime.now())
         
-    scheduler.add_job(tick, CronTrigger.from_crontab('* * * * *'))
+    tick_job = scheduler.add_job(tick, CronTrigger.from_crontab('* * * * *'))
     scheduler.start()
+    print('Next tick job scheduled at', tick_job.next_run_time)
+    
     try:
         # This is here to simulate application activity (which keeps the main thread alive).
         print('Press Ctrl+C to exit')
--- reverse_splitter/main.py
+++ reverse_splitter/main.py
@@ -4,8 +4,11 @@ import bin.logger as logger
 log = logger.setup_logger('Main')
 
 def main():
-    scheduler.schedule_jobs()
+    jobs = scheduler.schedule_jobs()
     scheduler.scheduler.start()
+    log.info(f'Next research job scheduled for {jobs[0].next_run_time}')
+    log.info(f'Next newsletter job scheduled for {jobs[1].next_run_time}')
+    
     try:
         log.debug('Entering infinite loop')
         log.debug('Press Ctrl+C to exit')
