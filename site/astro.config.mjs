e8f74d31da4b5a5c3959e0f19131278f6e697630
Sean <seantsusmilch@gmail.com>
2024-09-27 01:20:48 -0500

    add adapter

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
+
   experimental: {
     env: {
       schema: {
@@ -16,4 +19,8 @@ export default defineConfig({
       },
     },
   },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
