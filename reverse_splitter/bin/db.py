28021c1e4ec70a3d491ffebdd88e2a043047e28f
Sean <seantsusmilch@gmail.com>
2024-10-07 21:42:59 -0500

    Experimenting with yaHOO

--- reverse_splitter/bin/db.py
+++ reverse_splitter/bin/db.py
@@ -4,6 +4,7 @@ import cachetools.func
 
 @cachetools.func.ttl_cache(maxsize=1, ttl=1209600)
 def auth_pb():
+    print('Authenticating PocketBase')
     client = PocketBase(os.environ.get('PB_URL'))
     user = client.collection('users').auth_with_password(os.environ.get('PB_USER'), os.environ.get('PB_PASS'))
     return client, user.record
@@ -15,4 +16,7 @@ def get_user():
     return auth_pb()[1]
 
 if __name__ == '__main__':
+    print(vars(get_user()))
+    print(vars(get_user()))
+    print(vars(get_user()))
     print(vars(get_user()))
